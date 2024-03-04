e57d38682fa625899a6c3fadd76b22cd1f9822a9
phung hoang <lnphunghoang190903@gmail.com>
2024-03-04 09:05:25 +0700

    change branch2

--- BMITest/src/io/code/MainTest.java
+++ BMITest/src/io/code/MainTest.java
@@ -5,6 +5,11 @@ public class MainTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, Cocogoat");
+
+		int a = 4;
+		if (a <= 4){
+			System.out.println("Hi");
+		}
 	}
 
 }
